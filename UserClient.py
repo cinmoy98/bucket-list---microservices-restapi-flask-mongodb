22fd9accce2684031a2f375553e879e89def25e0
cinmoy98 <cinmoy98@gmail.com>
2021-09-04 16:57:02 +0600

    fix: endpoints updated and response, error handling fixed

--- UserClient.py
+++ UserClient.py
@@ -1,4 +1,5 @@
 import requests
+from flask import redirect,url_for
 from functools import wraps
 from flask_jwt_extended import decode_token
 from datetime import datetime
@@ -23,6 +24,8 @@ class UserClient:
 				return "Session expired. Log in again."
 			elif res == "Fresh token required":
 				return "Give your credentials again to continue."
+			elif res == "Token has been revoked":
+				return "Session expired. Log in again."
 			else:
 				return "Unknown error ! Try logging again."
 		elif code == 422:
@@ -38,12 +41,14 @@ class UserClient:
 		def wrapper(fn):
 			@wraps(fn)
 			def decorator(*args,**kwargs):
+				if global_var.tokens['access_token'] == None:
+					redirect(url_for('login'))
 				decoded_token = decode_token(global_var.tokens['access_token'], allow_expired=True)
 				exp_timestamp = decoded_token['exp']
 				now = datetime.now(timezone.utc)
 				target_timestamp = datetime.timestamp(now + timedelta(minutes=5))
 				if target_timestamp > exp_timestamp:
-					url = 'http://127.0.0.1:5000/api/user/refresh'
+					url = 'http://127.0.0.2:5000/api/user/refresh'
 					headers = {'Authorization': 'Bearer '+global_var.tokens["refresh_token"]}
 					response = requests.request("GET", url = url, headers=headers)
 					
@@ -62,7 +67,7 @@ class UserClient:
 
 	@staticmethod
 	def does_exist(username):
-		url = 'http://127.0.0.1:5000/api/user/'+username+'/exists'
+		url = 'http://127.0.0.2:5000/api/user/'+username+'/exists'
 		response = requests.request("GET", url=url)
 		return response.status_code == 200
 
@@ -78,27 +83,20 @@ class UserClient:
 		'lastname' : form.last_name.data,
 		'password' : form.password.data
 		}
-		print(payload)
-		url = 'http://127.0.0.1:5000/api/user/create'
+		url = 'http://127.0.0.2:5000/api/user/create'
 		response = requests.request("POST", url = url, data=payload)
 		if response:
-			user = 'CREATED'
-		return user
+			return response
 
 
 	@staticmethod
 	def post_login(form):
-		#global global_var.tokens
-		# tokens = {
-		# 'access_token' : None ,
-		# 'refresh_token' : None
-		# }
 		payload = {
 		'username' : form.username.data,
 		'password' : form.password.data
 		}
 
-		url = 'http://127.0.0.1:5000/api/user/login'
+		url = 'http://127.0.0.2:5000/api/user/login'
 		response = requests.request("POST", url=url, data=payload)
 		if response:
 			#UserClient.cookies = response.cookies
@@ -108,44 +106,41 @@ class UserClient:
 				'access_token' : dt['access_token'],
 				'refresh_token' : dt['refresh_token']
 				}
-			return "Login Successful"
+			return response.status_code
 
 	def check_if_logged_in():
 		if global_var.tokens['access_token'] == None:
-			print(global_var.tokens['access_token'])
 			return False
 		else:
 			return True
 
 	@staticmethod
 	def get_user():
-		
 		decoded_token = decode_token(global_var.tokens['access_token'], allow_expired=True)
 		return decoded_token['sub']
 
 
 	@staticmethod
-	@verify_token()
 	def check():
-		url = 'http://127.0.0.1:5000/protected'
-		#print(UserClient.cookies)
+		url = 'http://127.0.0.2:5000/protected'
 		headers = {'Authorization': 'Bearer '+global_var.tokens["access_token"]}
 		response = requests.request("GET", url = url, headers=headers)
 		if response.status_code == 200:
 			return response
 		else:
-			print(response.status_code)
-			return(UserClient.check_response_status_code(response))
+			print(response.json())
+			return response
+			#return(UserClient.check_response_status_code(response))
 
 	@staticmethod
-	@verify_token()
 	def logout():
-		url = "http://127.0.0.1:5000/api/user/logout"
+		url = "http://127.0.0.2:5000/api/user/logout"
 		headers = {'Authorization': 'Bearer '+global_var.tokens["access_token"]}
 		response = requests.request("POST", url = url, headers=headers)
 		if response:
-			dt = response.json()
-			if dt['msg'] == "logout-successful":
+			if response.status_code == 200:
 				global_var.tokens['access_token'] = None
 				global_var.tokens['refresh_token'] = None
-			return "Logout Successful"
+				return response.text
+			else:
+				return(UserClient.check_response_status_code(response))
